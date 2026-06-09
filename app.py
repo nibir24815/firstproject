935f76d63d658cc4bfbca6cad145680c5c680977
Nibir Baruah <nibirbaruah048@gmail.com>
2026-06-09 19:48:48 +0530

    Update logger and exception handling files

---
+++ app.py
@@ -0,0 +1,6 @@
+from src.ml_project.logger import logging
+from src.ml_project.exception import CustomException
+import sys
+if __name__=="__main__":
+    logging.info("the execution has started")
+    
---
+++ src/ml_project/exception.py
@@ -0,0 +1,22 @@
+import sys
+from src.ml_project.logger import logging
+def error_message_detail(error, error_detail: sys) -> str:
+    """
+    Extracts explicit file name and line number details for any given exception.
+    """
+    _, _, exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    line_number = exc_tb.tb_lineno
+    error_message = f"Error occurred in script: [{file_name}] at line: [{line_number}] -> Message: [{str(error)}]"
+    return error_message
+
+
+
+
+
+class CustomException(Exception):
+    def __init__(self,error_message,error_details:sys):
+        super().__init__(error_message)
+        self.error_message=error_message_detail(error_message,error_details)
+    def __str__(self):
+        return self.error_message                                            
